fd1361da0e7b095ba8b3646066b2058c7f8832ff
fagaye <fagayeseck11@gmail.com>
2023-11-28 20:49:14 -0500

    test

--- FrogGame/src/FrogGame.java
+++ FrogGame/src/FrogGame.java
@@ -1,7 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class FrogGame {
+public class FrogGame extends JFrame {
+ // private final AnimationPanel animationPanel;
 
-  public static void main(String[] args){
-    
+  public FrogGame() {
+    super("Frog-Nappers");
+    setSize(640, 480);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(() -> new FrogGame().setVisible(true));
   }
 }
